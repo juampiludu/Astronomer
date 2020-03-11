7d8fcc567ffe94b49ddcf25d867017ae4625bbf3
juampiludu <juanzakka@gmail.com>
2020-03-10 22:53:07 -0300

    all the planets all added an work correctly

---
+++ app/src/main/java/com/example/astronomer/Planets.kt
@@ -0,0 +1,16 @@
+package com.example.astronomer
+
+class Planets {
+
+    val earth = 9.807
+    val moon = 1.62
+    val mercury = 3.7
+    val venus = 8.87
+    val mars = 3.711
+    val jupiter = 24.79
+    val saturn = 10.44
+    val uranus = 8.87
+    val neptune = 11.15
+    val pluto = 0.62
+
+}
--- app/src/main/java/com/example/astronomer/ui/weight/WeightFragment.kt
+++ app/src/main/java/com/example/astronomer/ui/weight/WeightFragment.kt
@@ -9,7 +9,9 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import com.example.astronomer.Planets
 import com.example.astronomer.R
+import com.example.astronomer.Planets.*
 
 class WeightFragment : Fragment() {
 
@@ -20,21 +22,45 @@ class WeightFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        weightViewModel =
-                ViewModelProviders.of(this).get(WeightViewModel::class.java)
+        weightViewModel = ViewModelProviders.of(this).get(WeightViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_weight, container, false)
 
 
         val editWeight: EditText = root.findViewById(R.id.editWeight)
         val buttonWeight: Button = root.findViewById(R.id.buttonWeight)
+        val textMercury: TextView = root.findViewById(R.id.textMercury)
+        val textVenus: TextView = root.findViewById(R.id.textVenus)
         val textMars: TextView = root.findViewById(R.id.textMars)
+        val textJupiter: TextView = root.findViewById(R.id.textJupiter)
+        val textSaturn: TextView = root.findViewById(R.id.textSaturn)
+        val textUranus: TextView = root.findViewById(R.id.textUranus)
+        val textNeptune: TextView = root.findViewById(R.id.textNeptune)
+        val textPluto: TextView = root.findViewById(R.id.textPluto)
+        val textMoon: TextView = root.findViewById(R.id.textMoon)
+        val planets = Planets()
 
         buttonWeight.setOnClickListener {
 
             val weight = editWeight.text.toString()
             weight.toFloat()
-            val op = (weight.toFloat()*3.711) / 9.807
-            textMars.text = "$op kg"
+            val op1 = (weight.toFloat()*planets.mercury) / planets.earth
+            val op2 = (weight.toFloat()*planets.venus) / planets.earth
+            val op3 = (weight.toFloat()*planets.mars) / planets.earth
+            val op4 = (weight.toFloat()*planets.jupiter) / planets.earth
+            val op5 = (weight.toFloat()*planets.saturn) / planets.earth
+            val op6 = (weight.toFloat()*planets.uranus) / planets.earth
+            val op7 = (weight.toFloat()*planets.neptune) / planets.earth
+            val op8 = (weight.toFloat()*planets.pluto) / planets.earth
+            val op9 = (weight.toFloat()*planets.moon) / planets.earth
+            textMercury.text = "%.2f".format(op1) + " kg"
+            textVenus.text = "%.2f".format(op2) + " kg"
+            textMars.text = "%.2f".format(op2) + " kg"
+            textJupiter.text = "%.2f".format(op2) + " kg"
+            textSaturn.text = "%.2f".format(op2) + " kg"
+            textUranus.text = "%.2f".format(op2) + " kg"
+            textNeptune.text = "%.2f".format(op2) + " kg"
+            textPluto.text = "%.2f".format(op2) + " kg"
+            textMoon.text = "%.2f".format(op2) + " kg"
 
         }
 
