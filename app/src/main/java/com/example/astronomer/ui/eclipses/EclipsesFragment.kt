0433456ddfaaeb6c5eed9db444687047e5a1e000
juampiludu <juanzakka@gmail.com>
2020-03-11 10:44:49 -0300

    webviews added in eclipses_fragment

--- app/src/main/java/com/example/astronomer/ui/eclipses/EclipsesFragment.kt
+++ app/src/main/java/com/example/astronomer/ui/eclipses/EclipsesFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -22,6 +23,13 @@ class EclipsesFragment : Fragment() {
         eclipsesViewModel =
                 ViewModelProviders.of(this).get(EclipsesViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_eclipses, container, false)
+
+        val webView: WebView = root.findViewById(R.id.webView)
+        webView.loadUrl("file:///android_asset/myhtml.html")
+
+        val webView2: WebView = root.findViewById(R.id.webView2)
+        webView2.loadUrl("file:///android_asset/myhtml2.html")
+
         return root
     }
 }
