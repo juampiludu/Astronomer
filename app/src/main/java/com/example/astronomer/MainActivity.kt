251200d6faa71c4b35d8518afdb60884197b90bd
juampiludu <juanzakka@gmail.com>
2020-03-12 18:59:53 -0300

    reminder acivity ELIMINATED action to calendar event added

--- app/src/main/java/com/example/astronomer/MainActivity.kt
+++ app/src/main/java/com/example/astronomer/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.astronomer
 
-import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -22,14 +20,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
-
-        val fab: FloatingActionButton = findViewById(R.id.fab)
-        fab.setOnClickListener { view ->
-
-            val intent = Intent(this, ReminderActivity::class.java)
-            startActivity(intent)
-
-        }
 
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
--- app/src/main/java/com/example/astronomer/ui/eclipses/EclipsesFragment.kt
+++ app/src/main/java/com/example/astronomer/ui/eclipses/EclipsesFragment.kt
@@ -3,18 +3,16 @@ package com.example.astronomer.ui.eclipses
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.provider.CalendarContract
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.WebView
 import android.widget.ImageButton
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.astronomer.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 
 class EclipsesFragment : Fragment() {
 
@@ -29,6 +27,8 @@ class EclipsesFragment : Fragment() {
                 ViewModelProviders.of(this).get(EclipsesViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_eclipses, container, false)
 
+        Toast.makeText(this.context, "Press + to add an event", Toast.LENGTH_LONG).show()
+
         val imageButton: ImageButton = root.findViewById(R.id.imageButton)
 
         imageButton.setOnClickListener {
@@ -36,6 +36,18 @@ class EclipsesFragment : Fragment() {
             val openUrl =  Intent(android.content.Intent.ACTION_VIEW)
             openUrl.data = Uri.parse("https://www.timeanddate.com/eclipse/2020")
             startActivity(openUrl)
+
+        }
+
+        val fab: FloatingActionButton = root.findViewById(R.id.fab)
+
+        fab.setOnClickListener { view ->
+
+            val intent = Intent(Intent.ACTION_INSERT)
+                .setData(CalendarContract.Events.CONTENT_URI)
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, System.currentTimeMillis())
+                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, System.currentTimeMillis() + (60*60*1000))
+            startActivity(intent)
 
         }
 
