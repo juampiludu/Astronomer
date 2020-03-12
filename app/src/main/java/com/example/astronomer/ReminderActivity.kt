fc2fa8b7ba9e2bfe688adf7e5f1c6f3d62723036
juampiludu <juanzakka@gmail.com>
2020-03-12 00:04:52 -0300

    reminder activity added (nothing yet)

---
+++ app/src/main/java/com/example/astronomer/ReminderActivity.kt
@@ -0,0 +1,12 @@
+package com.example.astronomer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReminderActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reminder)
+    }
+}
