40c6c006c5013543ad33392db81d5d759265ec61
juampiludu <juanzakka@gmail.com>
2020-03-12 21:47:25 -0300

    version info added in config view

--- app/src/main/java/com/example/astronomer/ui/config/ConfigFragment.kt
+++ app/src/main/java/com/example/astronomer/ui/config/ConfigFragment.kt
@@ -4,6 +4,7 @@ import android.content.ContentValues.TAG
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.text.Html
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -13,6 +14,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.example.astronomer.BuildConfig
 import com.example.astronomer.R
 
 class ConfigFragment : Fragment() {
@@ -29,6 +31,11 @@ class ConfigFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_config, container, false)
 
         val emailButton: ImageButton = root.findViewById(R.id.emailButton)
+        val textView27: TextView = root.findViewById(R.id.textView27)
+        val textVersion: TextView = root.findViewById(R.id.textVersion)
+
+        textView27.text = Html.fromHtml(resources.getString(R.string.version_text))
+        textVersion.text = BuildConfig.VERSION_NAME
 
         emailButton.setOnClickListener {
 
