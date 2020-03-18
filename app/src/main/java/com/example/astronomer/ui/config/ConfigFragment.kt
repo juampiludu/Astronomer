7e29516886797afe5357d9d4aeaf14dda249d3cd
juampiludu <juanzakka@gmail.com>
2020-03-18 19:01:37 -0300

    aspects changed in config fragment

--- app/src/main/java/com/example/astronomer/ui/config/ConfigFragment.kt
+++ app/src/main/java/com/example/astronomer/ui/config/ConfigFragment.kt
@@ -7,7 +7,6 @@ import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
